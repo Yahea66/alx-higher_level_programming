debaf029cf51097bd147e18cb615f8d327f7ea32
Yahya Ahmed <yahya.clash8810@gmail.com>
2024-09-11 10:07:19 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 for i in range(9):
     for n in range(i + 1, 10):
-        if i != 8: 
-            print("{}{}".format(i,n), end=", ")
+        if i != 8:
+            print("{}{}".format(i, n), end=", ")
         else:
-            print("{}{}".format(i,n), end="")
-print() 
+            print("{}{}".format(i, n), end="")
+print()
