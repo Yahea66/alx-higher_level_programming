d740e4e2422dfd816cea2ebad5a2e6e4a1dd917e
Yahya Ahmed <yahya.clash8810@gmail.com>
2024-09-12 11:57:30 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,12 @@
 if __name__ == "__main__":
     import sys
     args = sys.argv
-    length = len(args) - 1
+    length = len(args) 
     if length != 1:
-        print("{} arguments:".format(length))
+        print("{} arguments:".format(length - 1))
     else:
-        print("{} argument:".format(length))
-    for i in range(length):
+        print("{} argument:".format(length - 1))
+    for i in range (length):
         if i == 0:
             continue
-        print("{}: {}".format(i, args[i]))
+        print("{}: {}".format(i, args[i])) 
