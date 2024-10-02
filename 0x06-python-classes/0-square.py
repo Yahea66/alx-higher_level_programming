543f63a69e3963bc08840701d71e0d69b89d5d47
Yahya Ahmed <yahya.clash8810@gmail.com>
2024-10-02 12:34:53 -0700

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+"""
+A module for defining a Square class.
+
+This module provides a simple example of a class definition
+intended to represent a square, with minimal attributes and methods.
+"""
+
 class Square:
+    """Class that defines a square.
+
+    At this stage, the class is just a basic skeleton
+    that contains no attributes or methods.
+    """
     pass
