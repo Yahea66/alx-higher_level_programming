855666db43d7f1d77760f12b58fa71fa5aaf0320
Yahya Ahmed <yahya.clash8810@gmail.com>
2024-10-09 07:25:16 -0700

    0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """Module 0-rectangle
 
-This module provides a class that defines a rectangle.
+This module defines an empty class Rectangle that serves as a placeholder
+to define a rectangle. This class can be expanded later with properties
+and methods that describe the characteristics and behaviors of a rectangle.
 """
 
 class Rectangle:
