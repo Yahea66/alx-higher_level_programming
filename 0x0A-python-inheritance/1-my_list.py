df78ca5fb211a887b2e66f6c4cb47fc28d71a8c4
Yahya Ahmed <yahya.clash8810@gmail.com>
2024-10-14 10:46:39 -0700

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+class MyList(list):
+    """
+    MyList extends the built-in Python list by adding a method to print the list
+    elements in sorted order without modifying the original list.
+    
+    Attributes:
+        Inherits all attributes of the built-in Python list.
+    
+    Methods:
+        print_sorted(): Prints the elements of the list in ascending order.
+    """
+
+    def print_sorted(self):
+        """
+        Prints the elements of the list in sorted order.
+        
+        This method does not alter the original list; instead, it prints a new list
+        that is sorted. The sorting is done in ascending order.
+        
+        Examples:
+            >>> my_list = MyList([3, 1, 2])
+            >>> my_list.print_sorted()
+            [1, 2, 3]
+        """
+        print(sorted(self))
