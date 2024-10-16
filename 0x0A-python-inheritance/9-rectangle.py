2038dcd0e7cf622eb0a1b8845ee957802d042196
Yahya Ahmed <yahya.clash8810@gmail.com>
2024-10-16 05:08:43 -0700

    9-rectangle.py

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+"""
+This module provides a Rectangle class that inherits from BaseGeometry
+"""
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """
+    Instantiation with width and height: def __init__(self, width, height):
+width and height must be private. No getter or setter
+width and height must be positive integers, validated by integer_validator
+    """
+
+    def __init__(self, width, height):
+        """
+        args:
+            width(int): the width of the rectangle
+            height(int): the height of the rectangle
+        """
+        
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height) 
+        self.__height = height
+
+    def area(self):
+        """
+        utility method to culculate the area of the rectangle
+        """
+
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        represents the rectangle dimensions 
+        """
+
+        return f"[Rectangle] {self.__width}/{self.__height}"
