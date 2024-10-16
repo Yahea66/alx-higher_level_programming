e08a1b1385c472d1524464d750d5c0ce45196280
Yahya Ahmed <yahya.clash8810@gmail.com>
2024-10-16 02:55:48 -0700

    5-base_geometry.py

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+This module provides a class that defines and Characterize different geometric structures 
+"""
+
+class BaseGeometry:
+    """
+    BaseGeometry defines the structure of a geometry utilize functions and attribute
+    """
+    pass
