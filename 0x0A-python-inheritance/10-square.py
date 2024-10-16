fd85f7e582da87f1a43093fd11bbc1e9e38377de
Yahya Ahmed <yahya.clash8810@gmail.com>
2024-10-16 05:20:43 -0700

    10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""
+This module provides a Rectangle class that inherits from BaseGeometry
+"""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+class Square(Rectangle):
+    """
+    Instantiation with size: def __init__(self, size)::
+
+size must be private. No getter or setter
+
+size must be a positive integer, validated by integer_validator
+    """
+
+    def __init__(self, size):
+        """
+        args:
+            size(int): the dimension of the Square 
+        """
+        
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """
+        utility method to culculate the area of the Square
+        """
+
+        return self.__size ** 2
+
+    def __str__(self):
+        """
+        represents the rectangle dimensions 
+        """
+
+        return f"[Rectangle] {self.__size}/{self.__size}"
