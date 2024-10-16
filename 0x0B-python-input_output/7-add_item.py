2521420ee5b05e067cf39f1054eb34633c0d617e
Yahya Ahmed <yahya.clash8810@gmail.com>
2024-10-16 13:57:09 -0700

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-
 """
-This module provides a set of functions to manage I/O operations 
+This module provides a set of functions to manage I/O operations.
 """
 
-import sys 
+import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = 'add_item.json'
-items = load_from_json_file(filename) 
+items = load_from_json_file(filename)
 items.extend(sys.argv[1:])
-
 save_to_json_file(items, filename)
