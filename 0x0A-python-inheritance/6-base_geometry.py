89c254964dd8b6c3723f0fa3c7c0e0a1c7e0c359
Yahya Ahmed <yahya.clash8810@gmail.com>
2024-10-16 03:04:18 -0700

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+This module provides a class that defines and Characterize different geometric structures 
+"""
+
+class BaseGeometry:
+    """
+    BaseGeometry defines the structure of a geometry utilize functions and attribute
+    """
+   
+    def area(self):
+        """
+        Public instance method: def area(self): that raises an Exception with the message area() is not implemented
+        """
+
+        raise Exception("area() is not implemented")
