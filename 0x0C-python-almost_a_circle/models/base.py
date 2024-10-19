0b56db0e8a917310b463850cf19040ce5e0beddd
Yahya Ahmed <yahya.clash8810@gmail.com>
2024-10-19 09:06:29 -0700

    models/base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,13 +1,23 @@
+#!/usr/bin/python3
+
+"""
+This module defines the Base class, which is designed to manage unique identifiers (IDs) for its instances.
+The IDs can either be provided by the user at the time of instantiation or automatically generated to ensure uniqueness.
+The automatic generation is handled through a class-level counter that increments with each new instance without a provided ID.
+"""
+
+import json
+
 class Base:
     """
     A class to manage IDs for instances, supporting either user-provided or automatically incremental IDs.
 
     Attributes:
-        _nb_objects (int): A class-level attribute that tracks how many instances have been created
-                           when no specific uid=0(root) gid=0(root) groups=0(root) is provided, ensuring unique IDs.
+        __nb_objects (int): A class-level attribute that tracks how many instances have been created
+                           when no specific ID is provided, ensuring unique IDs.
     """
 
-    _nb_objects = 0  # Use single underscore for less strict encapsulation
+    __nb_objects = 0
 
     def __init__(self, id=None):
         """
@@ -22,5 +32,21 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            Base._nb_objects += 1
-            self.id = Base._nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Converts a list of dictionaries into a JSON-formatted string. If the list is empty or not a list,
+        it returns an empty JSON array string "[]".
+
+        Parameters:
+            list_dictionaries (list): A list of dictionaries that should be converted into a JSON string.
+
+        Returns:
+            str: A JSON string representing the list of dictionaries. Returns "[]" if input is invalid or empty.
+        """
+        if not list_dictionaries or not isinstance(list_dictionaries, list):
+            return "[]"
+        return json.dumps(list_dictionaries)
