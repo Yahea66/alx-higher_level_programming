f3e8a29bf77a251cae15616cbcd2d6f82e111056
Yahya Ahmed <yahya.clash8810@gmail.com>
2025-01-01 16:11:50 -0800

    states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-this module is used to list all states from a specific mysql database.
-it uses MySQLdb to connect to the database and performs
-sql query to retrieve all states.
+This module is used to list all states from a specific MySQL database.
+It uses MySQLdb to connect to the database and perform
+an SQL query to retrieve all states.
 """
 
 import MySQLdb
@@ -11,28 +11,26 @@ import sys
 
 def main():
     """
-    Main function that uses command line argments
+    Main function that uses command line arguments
     to connect to a database and print all states in that database,
     sorted by their IDs.
     """
-
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
-    name_searched = sys.argv[4]
+    state_name_searched = sys.argv[4]
 
     db = MySQLdb.connect(
-            host="localhost",
-            user=mysql_username,
-            passwd=mysql_password,
-            db=database_name,
-            port=3306
-            )
+        host="localhost",
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name,
+        port=3306
+    )
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states
-                   WHERE name={} ORDER BY id ASC".format(name_searched))
-
+    query = "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC"
+    cursor.execute(query, (state_name_searched,))
     results = cursor.fetchall()
     for row in results:
         print(row)
