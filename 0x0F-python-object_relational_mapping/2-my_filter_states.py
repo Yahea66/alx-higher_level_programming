f44b9d03b64fcb1e68ab07c3b81db781e5affea7
Yahya Ahmed <yahya.clash8810@gmail.com>
2025-01-01 16:19:48 -0800

    states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,43 +1,19 @@
 #!/usr/bin/python3
-"""
-This module is used to list all states from a specific MySQL database.
-It uses MySQLdb to connect to the database and perform
-an SQL query to retrieve all states.
-"""
+"""Lists states"""
 
 import MySQLdb
-import sys
-
-
-def main():
-    """
-    Main function that uses command line arguments
-    to connect to a database and print all states in that database,
-    sorted by their IDs.
-    """
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_name_searched = sys.argv[4]
-
-    db = MySQLdb.connect(
-        host="localhost",
-        user=mysql_username,
-        passwd=mysql_password,
-        db=database_name,
-        port=3306
-    )
-
-    cursor = db.cursor()
-    query = "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC"
-    cursor.execute(query, (state_name_searched,))
-    results = cursor.fetchall()
-    for row in results:
-        print(row)
-
-    cursor.close()
-    db.close()
-
+from sys import argv
 
 if __name__ == "__main__":
-    main()
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    query = """
+SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC"""
+    query = query.format(argv[4])
+    cur.execute(query)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
