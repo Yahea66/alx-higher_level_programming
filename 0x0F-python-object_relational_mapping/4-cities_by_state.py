3169140b0aff3dfaf4e9a0e8f539b29e7ad28e95
Yahya Ahmed <yahya.clash8810@gmail.com>
2025-01-06 13:13:33 -0800

    cities

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Lists states"""
+
+import MySQLdb as db
+from sys import argv
+
+if __name__ == "__main__":
+    conn = db.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
+            charset="utf8"
+            )
+    cur = conn.cursor()
+    query = "SELECT * FROM cities ORDER BY cities.id ASC"
+    cur.execute(query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
