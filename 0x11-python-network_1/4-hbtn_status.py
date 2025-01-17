4707d13839eb2333803e1f2cd44a0f1c6f8414d6
Yahya Ahmed <yahya.clash8810@gmail.com>
2025-01-17 15:28:47 -0800

    requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""fetches https://alx-intranet.hbtn.io/status"""
+"""Fetches https://alx-intranet.hbtn.io/status"""
 import requests
 
 
-if __name__ == " __main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    response = requests.get(url)
+if __name__ == "__main__":
+    response = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {type(response.text)}")
-    print("\t- content: {response.text}")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
