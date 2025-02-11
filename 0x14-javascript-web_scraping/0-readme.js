c3ae070ff7d84ca14c1717e0052b98abc113b119
Yahya Ahmed <yahya.clash8810@gmail.com>
2025-02-11 14:03:16 -0800

    read a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 try {
-	const data = fs.readFileSync(filePath, 'utf-8')
-	console.log(data);
-	} catch (error) {
-		console.error(error);
-		process.exit(1);
-	}
+  const data = fs.readFileSync(filePath, 'utf-8');
+  console.log(data);
+} catch (error) {
+  console.error(error);
+  process.exit(1);
+}
